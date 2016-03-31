313c0d2acc40cb05c8129338aa5ad54cacd0fb45
dhackelman <drew.hackelman@gmail.com>
2016-03-31 16:02:52 -0500

    More minified pictures

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,43 @@
+var pngquant = require('imagemin-pngquant');
+var mozjpeg = require('imagemin-mozjpeg');
+var gifsicle = require('imagemin-gifsicle');
+
 module.exports = function(grunt) {
 
   // Project configuration.
+
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'dist/index.html': 'src/index.html',
+          'dist/views/pizza.html' : 'src/view/pizza.html'
+        }
+      }
+    },
 
     imagemin: {
-		jpg: {
-			options: {
-				progressive: true
-			},
-		files: [{
-			expand: true,
-			cwd: 'src/views/images/',
-			src: ['**/*.{png,jpg,gif}'],
-			dest: 'dist/views/images/'
-		}]
-		}   
+      target: {
+        options: {
+          optimizationLevel: 2, 
+          cache: false,
+          progressive: true, 
+          use: [pngquant(), mozjpeg(), gifsicle()]
+        },
+        files: [{
+          expand: true, 
+          cwd: 'src/views/images/',
+          src: ['**/*.{png,jpg,jpeg,fig}'],
+          dest: 'dist/views/images'
+        }]
+      }  
     },
 
     uglify: {
@@ -35,10 +57,14 @@ module.exports = function(grunt) {
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin-pngqunt');
+  grunt.loadNpmTasks('grunt-contrib-imagemin-mozjpeg');
+  grunt.loadNpmTasks('grunt-contrib-imagemin-gifsicle');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'uglify', 'imagemin']);
+  grunt.registerTask('default', ['htmlmin:dist','imagemin', 'uglify', 'uglify', ]);
 
 };
